71336bc0592f8e83c848b72d170bcc93431d5a50
bapibiswas100808 <star.bappy@gmail.com>
2024-03-15 22:43:28 +0600

    google fonts, food json, react icon ,image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import { FaHome } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-roboto font-bold underline">Hello world!</h1>
       <h1 className="text-3xl font-poppins font-bold underline">
-        Hello world!
+        Hello world! <FaHome />
       </h1>
     </>
   );
