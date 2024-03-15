5da4bd7aa7a34522c22e0444e91f8696b662f00f
bapibiswas100808 <star.bappy@gmail.com>
2024-03-15 22:09:33 +0600

    project started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-roboto font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-poppins font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
